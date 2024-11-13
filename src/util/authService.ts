7546496ecddbee0177e0328e819def0f9599be2e
Chubkey <69558928+Ravager6969@users.noreply.github.com>
2024-11-13 14:03:59 -0800

    feat: added authService to handle firebase authentication

---
+++ src/util/authService.ts
@@ -0,0 +1,35 @@
+// src/authService.js
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updateProfile, sendEmailVerification } from 'firebase/auth';
+import { auth } from '@/init/firebaseAuthInit';
+
+export const registerWithEmailAndPassword = async (email: string, password: string, displayName: string) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    
+    await updateProfile(userCredential.user, {displayName: displayName})
+    await sendEmailVerification(userCredential.user);
+    return userCredential.user;
+  } catch (error) {
+    console.error("Error registering:", error);
+    throw error;
+  }
+};
+
+export const loginWithEmailAndPassword = async (email: string, password: string) => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    return userCredential.user;
+  } catch (error) {
+    console.error("Error logging in:", error);
+    throw error;
+  }
+};
+
+export const logout = async () => {
+  try {
+    await signOut(auth);
+  } catch (error) {
+    console.error("Error logging out:", error);
+    throw error;
+  }
+};
