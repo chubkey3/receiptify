0f5a0979a775fc4608ceed51012eb751e179acc8
Chubkey <69558928+Ravager6969@users.noreply.github.com>
2024-11-13 14:06:20 -0800

    feat: convert img -> pdf with gcp functions url

---
+++ src/util/imgToPDF.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { getEnvVariable } from "./getEnvironmentalVariable"
+
+const convertURL = getEnvVariable("IMG_TO_PDF_URL");
+
+export async function imgToPDF(filename: string) {
+    try {
+        const response = await axios.post(convertURL, {filename: filename});
+        
+        return response.data;
+        
+    } catch (error) {
+        console.log(error);
+
+        return null;
+    }
+}
+
+
