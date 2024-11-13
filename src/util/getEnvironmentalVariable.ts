69c0a8537e2c54a7f1af6476bbc9920ca3e95e62
Chubkey <69558928+Ravager6969@users.noreply.github.com>
2024-11-13 14:05:49 -0800

    refactor: created function to centralize getting .env variables

---
+++ src/util/getEnvironmentalVariable.ts
@@ -0,0 +1,10 @@
+export function getEnvVariable(key: string) {
+    
+    const value = process.env[key];    
+    
+    if (value) {        
+        return value;
+    } else {
+        throw new Error(`Unable to find env variable of key ${key}`)
+    }
+}
