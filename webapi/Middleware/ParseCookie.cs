9e89f851f9dd693b88071b4d2127a89e37a4e186
Jason Louie <jason.jw.louie@gmail.com>
2025-07-12 05:24:52 -0700

    fix: removed middleware for cookie refresh route

--- webapi/Middleware/ParseCookie.cs
+++ webapi/Middleware/ParseCookie.cs
@@ -3,6 +3,7 @@ using FirebaseAdmin.Auth;
 public class ParseCookieMiddleware
 {
     private readonly RequestDelegate _next;
+    private readonly string[] _excludedPaths = ["/session"]; // Add your paths here
 
     public ParseCookieMiddleware(RequestDelegate next)
     {
@@ -12,22 +13,30 @@ public class ParseCookieMiddleware
     public async Task InvokeAsync(HttpContext context)
     {
 
-        if (context.Request.Cookies.TryGetValue("token", out var cookieValue))
-        {
-            try
-            {
-                var parsedUserId = await ParseCookie(cookieValue);
-                context.Items["userId"] = parsedUserId;
-            }
-            catch (Exception)
-            {
-                // Parsing failed (e.g., invalid format, expired token, etc.)
-                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-                await context.Response.WriteAsync("Expired cookie or invalid token");
-                return; // stop the pipeline
-            }
+        var requestPath = context.Request.Path.Value ?? "";
+        
+        if (_excludedPaths.Any(path => requestPath.StartsWith(path, StringComparison.OrdinalIgnoreCase)))
+        {            
+            await _next(context); // skip middleware
+            return;
+        }
 
-        }        
+        if (context.Request.Cookies.TryGetValue("token", out var cookieValue))
+            {
+                try
+                {
+                    var parsedUserId = await ParseCookie(cookieValue);
+                    context.Items["userId"] = parsedUserId;
+                }
+                catch (Exception)
+                {
+                    // Parsing failed (e.g., invalid format, expired token, etc.)
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    await context.Response.WriteAsync("Expired cookie or invalid token");
+                    return; // stop the pipeline
+                }
+
+            }        
 
         await _next(context);
     }
