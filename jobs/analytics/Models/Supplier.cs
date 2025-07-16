0f465173256e711b542587cc1d7e0fcb563635b3
Jason Louie <jason.jw.louie@gmail.com>
2025-07-15 21:53:47 -0700

    feat: added cache syncing job to update analytics summary data periodically

---
+++ jobs/analytics/Models/Supplier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace analytics.Models;
+
+[Table("supplier")]
+public partial class Supplier
+{
+    [Key]
+    [Column("supplier_id")]
+    public int SupplierId { get; set; }
+
+    [Required]
+    [MaxLength(255)]
+    public string SupplierName { get; set; } = null!;
+
+    public virtual ICollection<Expense> Expenses { get; set; } = new List<Expense>();
+}
